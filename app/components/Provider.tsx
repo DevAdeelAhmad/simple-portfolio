8941e45c61b04801d1e1d8ef2f9f432413784c77
DevAdeelAhmad <fa20-bse-031@cuilahore.edu.pk>
2023-08-02 15:42:45 +0500

    themebutton complete

--- app/components/Provider.tsx
+++ app/components/Provider.tsx
@@ -4,5 +4,5 @@ import { ThemeProvider } from "next-themes";
 import { ReactNode } from "react";
 
 export default function Provider({ children }: { children: ReactNode }) {
-    return <ThemeProvider>{children}</ThemeProvider>
+    return <ThemeProvider attribute="class">{children}</ThemeProvider>
 }
